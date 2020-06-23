9b3a8dea6ca981a5a6dba3783df12f3be3386490
Abdukhaligov <hikmat.pou@gmail.com>
2020-06-23 05:33:30 +0400

    Permission

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -4,14 +4,18 @@ import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepo userRepo;
@@ -27,6 +31,29 @@ public class UserController {
         data.put("user", user);
         data.put("roles", Role.values());
         return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String name,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user
+    ){
+        user.setName(name);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+
+        return "redirect:/user";
     }
 
 }
